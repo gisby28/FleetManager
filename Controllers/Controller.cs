cd3a7fc6e89af8c69d5d76ce009e9e46b8bed010
raydafi <rayan.dafi@outlook.com>
2025-11-10 12:20:23 +0100

    Commit 10/11/2025 12:20

---
+++ Controllers/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace FleetManager
+{
+    internal class Controller
+    {
+        private DataService dataService;
+
+        public Controller()
+        {
+            this.dataService = new DataService();
+        }
+
+        public List<User> GetTousLesUtilisateurs()
+        {
+            return dataService.SelectUsers();
+        }
+
+        public bool CreerNouvelUtilisateur(string username, string password, string role)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            return dataService.InsertUser(username, password, role);
+        }
+    }
+}
---
+++ Services/DataService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using MySqlConnector;
+
+namespace FleetManager
+{
+    internal class DataService
+    {
+        private string cnxString = "Server=localhost;Database=fleetmanager;Uid=root;Pwd=;";
+
+        // Méthode sélection users
+        public List<User> SelectUsers()
+        {
+            var userList = new List<User>();
+            string req = "SELECT user_id, username, password, role FROM users";
+
+            using (var msc = new MySqlConnection(cnxString))
+            {
+                try
+                {
+                    msc.Open();
+                    using (var cmd = new MySqlCommand(req, msc))
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var user = new User
+                            {
+                                UserId = reader.GetInt32("user_id"),
+                                Username = reader.GetString("username"),
+                                Password = reader.GetString("password"),
+                                Role = reader.GetString("role")
+                            };
+                            userList.Add(user);
+                        }
+                    }
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine("Erreur SQL (SelectUsers): " + e.Message);
+                }
+            }
+            return userList; 
+        }
+
+        // Méthode insertion users
+        public bool InsertUser(string username, string password, string role)
+        {
+            string req = "INSERT INTO users (username, password, role) VALUES (@username, @password, @role)";
+
+            using (var msc = new MySqlConnection(cnxString))
+            {
+                try
+                {
+                    msc.Open();
+                    using (var insert = new MySqlCommand(req, msc))
+                    {
+                        insert.Parameters.AddWithValue("@username", username);
+                        insert.Parameters.AddWithValue("@password", password);
+                        insert.Parameters.AddWithValue("@role", role);
+
+                        int rowsAffected = insert.ExecuteNonQuery();
+                        return rowsAffected > 0;
+                    }
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine("Erreur SQL (InsertUser): " + e.Message);
+                    return false;
+                }
+            }
+        }
+    }
+}
